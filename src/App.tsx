b9e746352f384b4b1972c73b4a391ab841a1d9d1
AasifSaneeth <alasaneeth22@gmail.com>
2025-04-19 14:34:14 -0400

    Cofigure api url with .env

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import reactLogo from './assets/react.svg'
 import LoginPage from './components/login'
 
 function App() {
+  const apiUrl = import.meta.env.VITE_API_URL;
+
 
   return (
+    
   <>
-    <LoginPage/>
+
+    {/* <LoginPage/> */}
+    {apiUrl}/register
   </>
   )
 }
