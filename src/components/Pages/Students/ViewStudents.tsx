7d52d1509ce2d4a1c2038f6bfd406844d81e8feb
AasifSaneeth <alasaneeth22@gmail.com>
2025-04-27 01:34:11 -0400

    Feat:Define grid view for viewStudent

--- src/components/Pages/Students/ViewStudents.tsx
+++ src/components/Pages/Students/ViewStudents.tsx
@@ -3,30 +3,33 @@ import StudentService from '../../../services/StudentService/Student.service';
 
 const ViewStudents = ({ onClose, id }: { onClose: () => void, id: any | null }) => {
 
-    const [data,setData] = useState({});
+    const [data, setData] = useState({});
 
-    useEffect(()=> {
+    useEffect(() => {
         const fetchStudentById = async () => {
             const res = await StudentService.get(id);
             setData(res)
         }
         fetchStudentById();
-    },[id])
-    
+    }, [id])
+
     return (
-    <div className="fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center bg-black/50 backdrop-blur-sm overflow-y-auto " >
-         <div className="relative p-4 w-full max-w-2xl max-h-[90vh] overflow-y-auto bg-white rounded-lg shadow-lg">
-           <div className='flex justify-end'>
-             <button
-               onClick={() => onClose()}>
-               X
-             </button>
-           </div>
-   
-           <h2 className="text-2xl font-bold mb-4">STUDENT REGISTRATION FORM</h2>
-         </div>
-       </div>
-  );
+        <div className="fixed top-0 left-0 w-full h-full z-50 flex justify-center items-center bg-black/50 backdrop-blur-sm overflow-y-auto " >
+            <div className="relative p-4 w-full max-w-2xl max-h-[90vh] overflow-y-auto bg-white rounded-lg shadow-lg">
+                <div className='flex justify-end'>
+                    <button
+                        onClick={() => onClose()}>
+                        X
+                    </button>
+                </div>
+                <h2 className="text-2xl font-bold mb-4">STUDENT REGISTRATION FORM</h2>
+                <div className="grid grid-cols-2 gap-4">
+                    <div className="p-4">Column 1</div>
+                    <div className="p-4">Column 2</div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ViewStudents;
